3ea62c7c0a9e493fcb79a0d78803071e301f6685
Marco Antonio Bidoia <mabidoia@veltec.com.br>
2018-04-19 09:07:00 -0300

    Adiciona documentacao dos models

--- src/main/kotlin/br/com/freezer/doc/model/Carro.kt
+++ src/main/kotlin/br/com/freezer/doc/model/Carro.kt
@@ -1,9 +1,31 @@
 package br.com.freezer.doc.model
 
 import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.TypeAlias
 
 @TypeAlias("carro")
-@ApiModel
-class Carro(@Id val id: Long, val placa: String, val ano: Int)
+@ApiModel("Veículo automotor")
+class Carro(
+    @ApiModelProperty(
+        value = "Identificador único do veículo",
+        example = "1",
+        required = true,
+        allowEmptyValue = false,
+        dataType = "Long",
+        readOnly = false,
+        name = "Id do veículo",
+        allowableValues = "range[1, ${Long.MAX_VALUE}]"
+    ) @Id val id: Long,
+    @ApiModelProperty(
+        value = "Placa do veículo",
+        example = "DRG8771"
+    ) val placa: String,
+    @ApiModelProperty(
+        example = "2005",
+        required = true,
+        allowEmptyValue = true,
+        dataType = "Integer"
+    ) val ano: Int
+)
--- src/main/kotlin/br/com/freezer/doc/model/Fabricante.kt
+++ src/main/kotlin/br/com/freezer/doc/model/Fabricante.kt
@@ -1,9 +1,23 @@
 package br.com.freezer.doc.model
 
 import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.TypeAlias
 
 @TypeAlias("fabricante")
 @ApiModel
-class Fabricante(@Id val id: Long, val marca: String)
+class Fabricante(
+    @ApiModelProperty(
+        example = "1",
+        position = 1
+    ) @Id val id: Long,
+    @ApiModelProperty(
+        example = "Nissan",
+        position = 2
+    ) val marca: String,
+    @ApiModelProperty(
+        example = "1981",
+        position = 3
+    ) val anoFundacao: String
+)
